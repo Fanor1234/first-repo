eb3510632437db54601dab38cea48b35122162e9
Fanor1234 <63176500+Fanor1234@users.noreply.github.com>
2023-04-11 12:20:08 -0400

    new file

---
+++ littlelemon/restaurant/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+#from django.contrib.auth.models import User
+
+# Create your models here.
+class BookingTable(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guest = models.IntegerField()
+    Booking_date = models.DateTimeField()
+
+
+#    def __str__(self):
+#    return self.name
+    
+    
+class MenuTable(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+
+#    def __str__(self):
+#        return self.name
